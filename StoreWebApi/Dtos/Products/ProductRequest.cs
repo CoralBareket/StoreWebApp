286bda37b99d893d95b3d7227ef9f6a02f9bfaca
Coral Bareket <coral.bareket@gmail.com>
2026-08-29 23:40:07 +0300

    allow zero product price

--- StoreWebApi/Dtos/Products/ProductRequest.cs
+++ StoreWebApi/Dtos/Products/ProductRequest.cs
@@ -12,7 +12,7 @@ public class ProductRequest
     [StringLength(100)]
     public string Category { get; set; } = string.Empty;
 
-    [Range(typeof(decimal), "0.01", "999999999")]
+    [Range(typeof(decimal), "0", "999999999")]
     public decimal Price { get; set; }
 
     [Range(0, int.MaxValue)]
