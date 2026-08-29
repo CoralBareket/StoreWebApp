0d8ddfcb0db225835b2c1825416fc9579e2d2b85
Coral Bareket <coral.bareket@gmail.com>
2026-08-29 17:19:08 +0300

    wip

--- StoreWebApi/Program.cs
+++ StoreWebApi/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using StoreWebApi.Models;
 using StoreWebApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 
 // Add services to the container.
 
