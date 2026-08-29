5f26df4f7035748c9b57626289fd828b0395d021
Coral Bareket <coral.bareket@gmail.com>
2026-08-29 21:51:42 +0300

    add PUT update product endpoint

--- StoreWebApi/Controllers/ProductsController.cs
+++ StoreWebApi/Controllers/ProductsController.cs
@@ -74,4 +74,35 @@ public class ProductsController : ControllerBase
 
         return StatusCode(StatusCodes.Status201Created, response);
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ProductResponse>> UpdateProduct(
+    int id,
+    ProductRequest request)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        product.Name = request.Name.Trim();
+        product.Category = request.Category.Trim();
+        product.Price = request.Price;
+        product.UnitsInStock = request.UnitsInStock;
+
+        await _context.SaveChangesAsync();
+
+        var response = new ProductResponse
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Category = product.Category,
+            Price = product.Price,
+            UnitsInStock = product.UnitsInStock
+        };
+
+        return Ok(response);
+    }
 }
