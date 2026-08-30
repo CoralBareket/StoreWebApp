bdc2f2f9ebb0c1ce0f312a1c084a710aac3a397a
Coral Bareket <coral.bareket@gmail.com>
2026-08-30 14:45:50 +0300

    wip: add products service

--- StoreApp/src/app/services/products.ts
+++ StoreApp/src/app/services/products.ts
@@ -1,4 +1,34 @@
-import { Service } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ProductRequest, ProductResponse } from '../models/product.models';
 
-@Service()
-export class Products {}
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'http://localhost:5249/api/products';
+
+  getProducts(search?: string) {
+    let params = new HttpParams();
+
+    if (search) {
+      params = params.set('search', search);
+    }
+
+    return this.http.get<ProductResponse[]>(this.apiUrl, {
+      params,
+    });
+  }
+
+  createProduct(request: ProductRequest) {
+    return this.http.post<ProductResponse>(this.apiUrl, request);
+  }
+
+  updateProduct(id: number, request: ProductRequest) {
+    return this.http.put<ProductResponse>(
+      `${this.apiUrl}/${id}`,
+      request,
+    );
+  }
+}
